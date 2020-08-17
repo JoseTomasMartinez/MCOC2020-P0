8066ce5b476adea7912d11a5e68c23f6cb5ba4c4
JoseTomasMartinez <53920966+JoseTomasMartinez@users.noreply.github.com>
2020-08-17 13:11:14 -0400

    Add files via upload

---
+++ Entrega 6/SolverParte2.py
@@ -0,0 +1,103 @@
+#Jose Tomas Martinez Lavin
+import scipy as sp
+import numpy as np
+from numpy import *
+from scipy import linalg
+from time import perf_counter
+import matplotlib.pyplot as plt
+def matriz_laplaciana(N,t=float32):
+	m=eye(N,N)-eye(N,N,1)
+	return t(m+m.T)
+
+solvers=["Inversa","NumpySolver","ScipySolver","SpSolverSymmetric","SpSolverPos","SpSolverPosOverwrite"]
+Corridas=10
+N = [2,5,10,12,15,20,30,40,45,50,55,60,75,100,125,160,200,250,350,500,600,800,1000,2000,3000,5000,7000,10000]
+archivos=[open(f"{n}.txt","w") for n in solvers]
+
+for i in range(len(N)):
+	dts=zeros((Corridas,len(solvers)))
+	for j in range(Corridas):
+
+		#Invertir y Multiplicar
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		Ainv=linalg.inv(A)
+		x=Ainv@b
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][0]=dt
+
+		#Numpy Solver
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		x=np.linalg.solve(A,b)
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][1]=dt
+
+		#Scipy Solver
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		x=sp.linalg.solve(A,b)
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][2]=dt
+
+		#Scipy Solver Symmetric
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		x=sp.linalg.solve(A,b,sym_pos=True)
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][3]=dt
+
+		#Scipy Solver Positive
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		x=sp.linalg.solve(A,b,assume_a="pos")
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][4]=dt
+
+		#Scipy Solver Positive Overwrite
+		A=matriz_laplaciana(N[i])
+		b=ones(N[i])
+		t1=perf_counter()
+		x=sp.linalg.solve(A,b,assume_a="pos",overwrite_a=True,overwrite_b=True)
+		t2=perf_counter()
+		dt=t2-t1
+		dts[j][5]=dt
+
+	promedio=[mean(dts[:,p]) for p in range(len(archivos))]
+	for j in range(len(archivos)):
+		archivos[j].write(f"{promedio[j]}\n")
+[a.close() for a in archivos]
+
+T=[]
+for s in solvers:
+	t=[]
+	a=open(f"{s}.txt")
+	for i in a:
+		t.append(float(i))
+	T.append(t)
+
+plt.title("Rendimiento Solvers")
+plt.ylabel("Tiempo Transcurrido (s)")
+plt.xscale("log")
+plt.yscale("log")
+plt.grid()
+tickt=[0.0001,0.001,0.01,0.1,1,10,60,600]
+ticktstr=["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"]
+tickn=[10,20,50,100,200,500,1000,2000,5000,10000,20000]
+plt.yticks(tickt,ticktstr)
+plt.xticks(tickn,tickn,rotation=75)
+for i in range(len(solvers)):
+	plt.plot(N,T[i],"o-")
+plt.legend(solvers)
+plt.savefig("Rendimiento Solvers Parte2")	
+plt.show()
